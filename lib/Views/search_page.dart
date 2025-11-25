663e086b1c1cb2ee5b435a59fff2a6bdb100241a
Ciffedinne <ciffedinnemahdjoub@gmail.com>
2025-11-25 20:07:21 +0100

    Ajout de la barre d'application Miaam complète avec logo, titre et heure

--- lib/Views/search_page.dart
+++ lib/Views/search_page.dart
@@ -6,6 +6,31 @@ class SearchPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFF6F8F4),
-      appBar: _buildAppBar(context),
+      appBar: _buildAppBar(context),);
   }
 }
+
+PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(60),
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: Row(
+            children: [
+              Image.asset(
+                'assets/images/logo.png',
+                width: 32,
+                height: 32,
+                errorBuilder: (_, __, ___) => const Icon(Icons.restaurant, size: 32, color: Color(0xFF6B8E23)),
+              ),
+              const SizedBox(width: 8),
+              const Text("Miaam", style: TextStyle(color: Color(0xFF6B8E23), fontWeight: FontWeight.w700, fontSize: 22)),
+              const Spacer(),
+              Text(TimeOfDay.now().format(context), style: const TextStyle(color: Colors.black38)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
