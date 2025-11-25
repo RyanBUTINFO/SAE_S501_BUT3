9c212246e3f39b73e57b1f9b4691e90fc3052e57
Ciffedinne <ciffedinnemahdjoub@gmail.com>
2025-11-25 19:38:59 +0100

    Ajout du background Miaam et de l'AppBar

--- lib/Views/search_page.dart
+++ lib/Views/search_page.dart
@@ -4,6 +4,8 @@ class SearchPage extends StatelessWidget {
   const SearchPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xFFF6F8F4),
+      appBar: _buildAppBar(context),
   }
 }
