79bf89a5092d4b46642316bc28437455737a9228
Ciffedinne <ciffedinnemahdjoub@gmail.com>
2025-12-01 20:40:46 +0100

    ajout d'une fonction qui recupere tout les plats

--- lib/controllers/recommendation_controller.dart
+++ lib/controllers/recommendation_controller.dart
@@ -1,5 +1,3 @@
-// lib/controllers/recommendation_controller.dart
-
 import 'package:flutter/foundation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../models/plat.dart';
